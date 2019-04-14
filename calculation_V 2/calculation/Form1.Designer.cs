d941cb7a2bf0e1adabf9cdb6d0d039f94c971aea
dl_None <41817051+dlNone@users.noreply.github.com>
2019-04-14 20:36:19 +0800

    Add files via upload
    可在软件上做题，可将试题导出到word中

---
+++ calculation_V 2/calculation/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(751, 12);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(255, 374);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(195, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(166, 53);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 15);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "请输入算术题数量：";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(269, 47);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "导出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(432, 303);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "序号";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "题目";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 140;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "答案";
+            this.Column3.Name = "Column3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "算术题显示答案";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 398);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "算术题生成器";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ calculation_V 2/calculation/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace calculation
+{
+    
+    public partial class Form1 : Form
+    {
+        String[] shizi = new String[10000];
+        bool ischanged = true;
+
+        public object Application { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //生成按钮点击事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ischanged = false;
+            if (textBox2.Text == "")
+            {
+                textBox1.Text = "";
+                MessageBox.Show("未输入生成计算题数量！");
+            }                
+            else
+            {
+                textBox1.Text = "";
+                int num;
+                generate gen = new generate();
+                num = Convert.ToInt32(textBox2.Text);
+                shizi = gen.fun(num);
+                //显示答案
+                if (checkBox1.CheckState == CheckState.Checked)
+                {
+                    dataGridView1.Rows.Clear();                    
+                    for (int i = 0; i < num; i++)
+                    {
+                        String[] str = shizi[i].Split('=');
+                        int index = this.dataGridView1.Rows.Add();
+                        this.dataGridView1.Rows[index].Cells[0].Value = i + 1;
+                        this.dataGridView1.Rows[index].Cells[1].Value = str[0] + '=';
+                        this.dataGridView1.Rows[index].Cells[2].Value = str[1];
+
+                    }
+                }
+                    
+                //不显示答案        
+                else
+                {
+                    dataGridView1.Rows.Clear();
+                    for (int i = 0; i < num; i++)
+                    {
+                        String[] str = shizi[i].Split('=');                       
+                        int index = this.dataGridView1.Rows.Add();
+                        this.dataGridView1.Rows[index].Cells[0].Value = i+1;
+                        this.dataGridView1.Rows[index].Cells[1].Value = str[0] + '=';
+
+                    }                       
+                }                
+                
+            }
+            ischanged = true;
+
+        }
+        //限制只能输入数字
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {          
+            if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8)          
+                e.Handled = true;
+        }
+        //导出按钮点击事件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            if(textBox2.Text != "")
+                num = Convert.ToInt32(textBox2.Text);
+            
+            //创建Word文档
+            Document doc = new Document();
+            //添加section
+            Section section = doc.AddSection();
+
+            //添加表格
+            Table table = section.AddTable(true);
+                
+            //添加第1行
+            TableRow row1 = table.AddRow();
+            TableCell cell1 = row1.AddCell();
+            cell1.AddParagraph().AppendText("题 号");
+            cell1.CellFormat.BackColor = Color.Gold;
+            TableCell cell2 = row1.AddCell();
+            cell2.AddParagraph().AppendText("题 目");
+            
+            TableCell cell3 = row1.AddCell();
+            cell3.AddParagraph().AppendText("题 号");
+            cell3.CellFormat.BackColor = Color.Gold;
+            TableCell cell4 = row1.AddCell();
+            cell4.AddParagraph().AppendText("题 目");
+            
+            TableCell cell5 = row1.AddCell();
+            cell5.AddParagraph().AppendText("题 号");
+            cell5.CellFormat.BackColor = Color.Gold;
+            TableCell cell6 = row1.AddCell();
+            cell6.AddParagraph().AppendText("题 目");
+            
+            TableCell cell7 = row1.AddCell();
+            cell7.AddParagraph().AppendText("题 号");
+            cell7.CellFormat.BackColor = Color.Gold;
+            TableCell cell8 = row1.AddCell();
+            cell8.AddParagraph().AppendText("题 目");
+            
+            int j = 0;
+            for (int i = 0; i < num / 4 + 1; i++)
+            {                    
+                TableRow row = table.AddRow();
+                j++;
+                if(j < num + 1)
+                {
+                    table.Rows[i + 1].Cells[0].AddParagraph().AppendText(j.ToString());
+                    table.Rows[i + 1].Cells[0].CellFormat.BackColor = Color.Gold;
+                    table.Rows[i + 1].Cells[1].AddParagraph().AppendText(shizi[j - 1]);                  
+                }                    
+                j++;
+                if (j < num + 1)
+                {
+                    table.Rows[i + 1].Cells[2].AddParagraph().AppendText(j.ToString());
+                    table.Rows[i + 1].Cells[2].CellFormat.BackColor = Color.Gold;
+                    table.Rows[i + 1].Cells[3].AddParagraph().AppendText(shizi[j - 1]);                 
+                }
+                    
+                j++;
+                if (j < num + 1)
+                {
+                    table.Rows[i + 1].Cells[4].AddParagraph().AppendText(j.ToString());
+                    table.Rows[i + 1].Cells[4].CellFormat.BackColor = Color.Gold;
+                    table.Rows[i + 1].Cells[5].AddParagraph().AppendText(shizi[j - 1]);
+                }
+                j++;
+                if (j < num + 1)
+                {
+                    table.Rows[i + 1].Cells[6].AddParagraph().AppendText(j.ToString());
+                    table.Rows[i + 1].Cells[6].CellFormat.BackColor = Color.Gold;
+                    table.Rows[i + 1].Cells[7].AddParagraph().AppendText(shizi[j - 1]);
+                }
+            }
+            //保存文档
+            table.AutoFit(AutoFitBehaviorType.FixedColumnWidths);
+            doc.SaveToFile("Table.docx");
+            MessageBox.Show("导出成功！");                     
+        }
+        //是否显示答案
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ischanged = false;
+            textBox1.Text = "";
+            int num = 0;
+            try
+            {                
+                num = Convert.ToInt32(textBox2.Text);
+            }
+            catch { }            
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < num; i++)
+                {
+                    String[] str = shizi[i].Split('=');
+                    int index = this.dataGridView1.Rows.Add();
+                    this.dataGridView1.Rows[index].Cells[0].Value = i + 1;
+                    this.dataGridView1.Rows[index].Cells[1].Value = str[0] + '=';
+                    this.dataGridView1.Rows[index].Cells[2].Value = str[1];
+                }
+            }                                   
+            else
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < num; i++)
+                {
+                    String[] str = shizi[i].Split('=');
+                    int index = this.dataGridView1.Rows.Add();
+                    this.dataGridView1.Rows[index].Cells[0].Value = i + 1;
+                    this.dataGridView1.Rows[index].Cells[1].Value = str[0] + '=';
+                }
+            }
+            ischanged = true;
+        }
+        //表格中填写答案
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if(ischanged)
+                if(e.ColumnIndex == 2 && e.RowIndex > -1)
+                {
+
+                    string msg = String.Format("Cell at row {0}, column {1} value changed", e.RowIndex, e.ColumnIndex);
+                    if (this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() != shizi[e.RowIndex].Split('=')[1])
+                    {
+                        dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Yellow;
+                        MessageBox.Show("答案错误！");
+                    }
+                    else
+                        dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.White;
+                }
+        }
+    }
+}
